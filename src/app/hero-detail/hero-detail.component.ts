a2ecf6c088acae652e85ef3a472a54788e41abb4
Luke Vandekieft <vandekie@gmail.com>
2022-12-28 10:17:06 -0800

    Create async timeout function.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,8 +33,32 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
- save(): void {
+  save(): void {
     this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
+  }
+
+  // Observables ARE async, but since they can be easily triggered immediately
+  // they don't feel that way for unit tests
+  saveAsync(): void {
+    this.debounce(() => {
+      this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+    }, 250, false);
+  }
+
+  debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if(!immediate) func.apply(context, args);
+      }
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if(callNow) func.apply(context, args);
+    }
   }
 }
