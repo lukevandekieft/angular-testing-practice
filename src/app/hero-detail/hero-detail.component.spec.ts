11da0aa0a43e04863e70912ae84c30fefcbb54c9
Luke Vandekieft <vandekie@gmail.com>
2022-12-27 16:22:32 -0800

    Test ActivatedRoute.

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,10 +1,12 @@
-import { TestBed } from "@angular/core/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { FormsModule } from "@angular/forms"
 import { ActivatedRoute } from "@angular/router"
+import { of } from "rxjs"
 import { HeroService } from "../hero.service"
 import { HeroDetailComponent } from "./hero-detail.component"
 
 describe('HeroDetail', () => {
-  let fixture;
+  let fixture: ComponentFixture<HeroDetailComponent>;
   let mockActivatedRoute;
   let mockHeroService;
   let mockLocation;
@@ -18,6 +20,7 @@ describe('HeroDetail', () => {
 
     TestBed.configureTestingModule({
       declarations: [HeroDetailComponent],
+      imports: [FormsModule],
       providers: [
         {provide: ActivatedRoute, useValue: mockActivatedRoute},
         {provide: HeroService, useValue: mockHeroService},
@@ -26,5 +29,13 @@ describe('HeroDetail', () => {
     })
 
     fixture = TestBed.createComponent(HeroDetailComponent);
+
+    mockHeroService.getHero.and.returnValue(of({ id: 3, name: "Gandhi", strength: 4}));
+  })
+
+  it('should render hero name in a h2 tag', () => {
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain('GANDHI');
   })
 })
